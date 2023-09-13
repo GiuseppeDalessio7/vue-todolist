73b0aa0e02d5e701289bceebccb52677e328dea6
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-09-13 17:16:13 +0200

    Push for help

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -6,6 +6,7 @@ createApp({
     data() {
         return {
             newtask :"",
+            messggio:"",
             todos: [
                 {
                     text: "Fare la spesa",
@@ -25,15 +26,19 @@ createApp({
                     text: "Non imparare il vuejs",
                     done: false,
                 },
+                // {
+                    // text: "leggere un libro",
+                    // done: true,
+                // },
             ]
         }
     },
 
     methods: {
         addTask(){
-          if(this.newTask != ""){
-           
-          }  
+            if(this.newtask === this.newtask)
+         todos.text.push(todo.done)
+          
         },
 
         //funzione elimina che elimina il todo
@@ -49,11 +54,16 @@ createApp({
 
 /*
  
+MILESTONE 3 Predisporre un campo di input testuale e un pulsante 
+“aggiungi”: cliccando sul pulsante, il testo digitato viene letto e 
+utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista 
+dei todo esistenti.
+
  Rifare l’esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 text, una stringa che indica il testo del todo
 done, un booleano (true/false) che indica se il todo è stato fatto oppure no
  
 MILESTONE 1 Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
-MILESTONE 3 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 */
